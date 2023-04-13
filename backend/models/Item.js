d5f114b6853765197dd43669584b382c0873131c
WilcoApp <hello@wilco.gg>
2023-04-13 10:52:09 +0000

    Robocrop

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -6,20 +6,20 @@ var User = mongoose.model("User");
 var ItemSchema = new mongoose.Schema(
   {
     slug: { type: String, lowercase: true, unique: true },
-    title: String,
-    description: String,
+    title: {type: String, required: [true, "can't be blank"]},
+    description: {type: String, required: [true, "can't be blank"]},
     image: String,
     favoritesCount: { type: Number, default: 0 },
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
     tagList: [{ type: String }],
-    seller: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    seller: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
   },
   { timestamps: true }
 );
 
 ItemSchema.plugin(uniqueValidator, { message: "is already taken" });
 
-ItemSchema.pre("validate", function (next) {
+ItemSchema.pre("validate", function(next) {
   if (!this.slug) {
     this.slugify();
   }
@@ -27,36 +27,35 @@ ItemSchema.pre("validate", function (next) {
   next();
 });
 
-ItemSchema.methods.slugify = function () {
+ItemSchema.methods.slugify = function() {
   this.slug =
     slug(this.title) +
     "-" +
     ((Math.random() * Math.pow(36, 6)) | 0).toString(36);
 };
 
-ItemSchema.methods.updateFavoriteCount = function () {
+ItemSchema.methods.updateFavoriteCount = function() {
   var item = this;
 
-  return User.count({ favorites: { $in: [item._id] } }).then(function (count) {
+  return User.count({ favorites: { $in: [item._id] } }).then(function(count) {
     item.favoritesCount = count;
 
     return item.save();
   });
 };
 
-ItemSchema.methods.toJSONFor = function (user) {
-  let image = this.image ? this.image : "/Users/keitadarby/projects/Anythink-Market-mf23i8sb/backend/placeholder.png";
+ItemSchema.methods.toJSONFor = function(user) {
   return {
     slug: this.slug,
     title: this.title,
     description: this.description,
-    image: image,
+    image: this.image,
     createdAt: this.createdAt,
     updatedAt: this.updatedAt,
     tagList: this.tagList,
     favorited: user ? user.isFavorite(this._id) : false,
     favoritesCount: this.favoritesCount,
-    seller: this.seller.toProfileJSONFor(user),
+    seller: this.seller.toProfileJSONFor(user)
   };
 };
 
